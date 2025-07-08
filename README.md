6b3b80b3054afe9d33314a9c17591e4fc6816012
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-07-08 23:31:33 +0800

    chore: `pnpm create preact`

---
+++ README.md
@@ -0,0 +1,1 @@
+# typst-set-font
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    preact({
+      prerender: {
+        enabled: true,
+        renderTarget: "#app",
+      },
+    }),
+  ],
+});
