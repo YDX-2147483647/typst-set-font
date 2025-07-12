3dd5342afe9ddea6977e3a243624f47d446daf79
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-07-13 00:13:50 +0800

    fix: Make base URL relative

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
+  base: "./", // https://vite.dev/guide/build.html#relative-base
   css: {
     postcss: {
       plugins: [postcssNesting()],
