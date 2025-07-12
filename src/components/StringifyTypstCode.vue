060846a7b4da0c454a034ff3c9a5aded66981075
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-07-12 13:06:05 +0800

    feat: Format the code with typstyle

--- src/components/StringifyTypstCode.vue
+++ src/components/StringifyTypstCode.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { NRadioButton, NRadioGroup } from "naive-ui";
+import { pretty_print_wasm } from "typstyle-core";
 import { computed, ref } from "vue";
 import { calc_advanced, stringify_advanced } from "../fonts/advanced.ts";
 import { stringify_FontSet } from "../fonts/as_typst.ts";
@@ -13,10 +14,13 @@ const { font } = defineProps<{
 const mode = ref<"markup" | "code">("markup");
 
 const code = computed(() =>
-  stringify_FontSet(resolve_FontSet(font), {
-    mode: mode.value,
-    afterwords: stringify_advanced(calc_advanced(font)),
-  }),
+  pretty_print_wasm(
+    stringify_FontSet(resolve_FontSet(font), {
+      mode: mode.value,
+      afterwords: stringify_advanced(calc_advanced(font)),
+    }) || "// 未设置字体",
+    80,
+  ).trim(),
 );
 </script>
 
@@ -31,7 +35,7 @@ const code = computed(() =>
       />
     </n-radio-group>
     <pre><code class="block overflow-x-auto max-w-4xs">{{
-      code || "// 未设置字体"
+      code
     }}</code></pre>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import postcssNesting from "postcss-nesting";
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +12,6 @@ export default defineConfig({
       plugins: [postcssNesting()],
     },
   },
-  plugins: [tailwindcss(), vue()],
+  plugins: [tailwindcss(), vue(), wasm(), topLevelAwait()],
+  // wasm and topLevelAwait are required by typstyle-core.
 });
